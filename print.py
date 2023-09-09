9aece8add1337678988684667eb8daa3dc19aeb1
kosyakarseniy20080 <144513307+kosyakarseniy20080@users.noreply.github.com>
2023-09-09 16:38:35 +1000

    Create print.py
    Файл с описанием команды print в Python

---
+++ print.py
@@ -0,0 +1,17 @@
+print('Первая строчка\n','Hello WORLD!', "Call of Duty",sep=' пробел ',end='\n')
+print('Вторая строка')
+print(56*1000)
+number = 10
+print(number)
+name = '771Ak232'
+print('Мой никмейм',name, 'текст','eщё текст', number )
+print(f'Мой никнейм {name} текст ещё текст {number}')
+a = 100
+b = 200
+ 
+print('Сложение',a+b)
+print('Вычитание',a-b)
+print('Умножение',a*b)
+print('Деление',a/b)
+print('Целочисленное деление',a//b)
+print('Остаток от деления',a%b)
